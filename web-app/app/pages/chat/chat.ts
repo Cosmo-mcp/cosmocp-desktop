adba27e4f5f1bf6920a683097d8d26e04a72a36b
shashank-cosmo <shashank@cosmocp.com>
2025-07-12 00:28:50 +0530

    - moved pages to app/pages - created home and chat pages - added temporary html and scss

---
+++ web-app/app/pages/chat/chat.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'co-chat',
+  imports: [RouterModule],
+  templateUrl: './chat.html',
+  styleUrl: './chat.scss'
+})
+export class Chat {
+
+}
