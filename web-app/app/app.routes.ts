ef3143c8dfe15395c79fec12b2ec1528ae223e22
shashank-cosmo <shashank@cosmocp.com>
2025-07-17 23:01:33 +0530

    Removing angular

--- web-app/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home'
-import { Chat } from './pages/chat/chat'
-
-export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'chat', component: Chat}
- ];
