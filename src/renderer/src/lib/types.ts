570b2f3153e4928cd64572e0c0e92ef6fb65a59b
Cosmo-mcp <gaurav@cosmocp.com>
2025-07-31 05:49:15 +0530

    Added types and updated utils file to include generateUUID
    Created types for chat message and attachment

---
+++ src/renderer/src/lib/types.ts
@@ -0,0 +1,29 @@
+import type {UIMessage} from 'ai';
+import {z} from "zod";
+
+export const messageMetadataSchema = z.object({
+    createdAt: z.string(),
+});
+
+export type MessageMetadata = z.infer<typeof messageMetadataSchema>;
+
+export type CustomUIDataTypes = {
+    textDelta: string;
+    imageDelta: string;
+    sheetDelta: string;
+    codeDelta: string;
+    appendMessage: string;
+    id: string;
+    title: string;
+    clear: null;
+    finish: null;
+};
+
+
+export type ChatMessage = UIMessage<MessageMetadata, CustomUIDataTypes>;
+
+export interface Attachment {
+    name: string;
+    url: string;
+    contentType: string;
+}
