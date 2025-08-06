3f10f1feda197b0527927c1217eaca43ebe5a6b5
Cosmo-mcp <gaurav@cosmocp.com>
2025-08-06 05:32:40 +0530

    Added dropdown to select registered model (#16)
    Added a dropdown that will show registered model coming from the main process.

--- src/renderer/src/app/page.tsx
+++ src/renderer/src/app/page.tsx
@@ -1,72 +1,18 @@
 'use client'
-
-import {UIMessage, useChat} from '@ai-sdk/react'
-import {JSX, useState} from "react";
-import {ipcChatTransport} from '@/chat-transport';
-
-const initialMessages: UIMessage[] = [
-    {
-        id: '1',
-        parts: [{
-            type: 'text',
-            text: 'Hello! How can I help you today?'
-        }],
-        role: 'assistant',
-    },
-]
+import {JSX} from "react";
+import {generateUUID} from "@/lib/utils";
+import {Chat} from "@/components/chat";
 
 export default function Page(): JSX.Element {
-    return (
-        <div className="flex h-screen flex-col">
-            <header className="w-full border-b p-4 text-center">
-                <h1 className="text-2xl font-bold">
-                    Cosmo MCP
-                </h1>
-            </header>
-            <div className="min-h-0 flex-1">
-                <ChatExample/>
-            </div>
-        </div>
-    )
-}
-
-function ChatExample() {
-    const {messages, sendMessage} = useChat({
-        transport: ipcChatTransport,
-        messages: initialMessages
-    })
-    const [input, setInput] = useState('');
-
+    const id = generateUUID();
     return (
         <>
-            {messages.map(message => (
-                <div key={message.id}>
-                    {message.role === 'user' ? 'User: ' : 'AI: '}
-                    {message.parts.map((part, index) =>
-                        part.type === 'text' ? <span key={index}>{part.text}</span> : null,
-                    )}
-                </div>
-            ))}
-
-            <form
-                onSubmit={e => {
-                    e.preventDefault();
-                    if (input.trim()) {
-                        sendMessage({text: input}).then(r => console.log(r));
-                        setInput('');
-                    }
-                }}
-            >
-                <input
-                    name="message"
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                    placeholder="Say something..."
-                />
-                <button type="submit">
-                    Submit
-                </button>
-            </form>
+            <Chat
+                key={id}
+                id={id}
+                initialMessages={[]}
+                initialChatModel='gemini-2.0-flash-lite'
+            />
         </>
-    )
+    );
 }
