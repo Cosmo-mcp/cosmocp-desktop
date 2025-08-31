81b91726b05e7946916a6c75319dc3785df1227f
Cosmo-mcp <gaurav@cosmocp.com>
2025-08-31 15:16:27 +0530

    fix

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from 'vite';
 import path from "path";
 
 export default defineConfig({
-  root: './src/renderer/out/', // This tells Vite where to find index.html
-  build: {
-      outDir: path.resolve(__dirname, '.vite/renderer/main_window'), // This is where Vite outputs the renderer assets
-  },
+    root: './src/renderer/out/', // This tells Vite where to find index.html
+    build: {
+        outDir: path.resolve(__dirname, '.vite/renderer/main_window'), // This is where Vite outputs the renderer assets
+    },
 });
