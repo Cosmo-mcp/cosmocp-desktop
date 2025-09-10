447c398e12ef348b7374243f27cabbd6e66ee424
Cosmo-mcp <gaurav@cosmocp.com>
2025-09-10 05:58:20 +0530

    Fixed Electron forge build  (#31)
    Co-authored-by: jsuryakt-cosmo <jayasurya@cosmocp.com>

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
+import path from "path";
 
 export default defineConfig({
-  root: './src/renderer', // This tells Vite where to find index.html
-  build: {
-    outDir: '.vite/renderer/main_window', // This is where Vite outputs the renderer assets
-  },
+    root: './src/renderer/out/', // This tells Vite where to find index.html
+    build: {
+        outDir: path.resolve(__dirname, '.vite/renderer/main_window'), // This is where Vite outputs the renderer assets
+    },
 });
