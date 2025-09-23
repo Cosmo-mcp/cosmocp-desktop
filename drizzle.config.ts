470b249ff50b13796a827c802c97cf413b66dfee
Cosmo-mcp <gaurav@cosmocp.com>
2025-09-23 20:19:45 +0530

    Cosmo 40: Drizzle (#36)
    * Added Drizze ORM and PGLITE
    
    * Add type for highgligter
    
    * Drizzle and PGLite Config

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import {defineConfig} from "drizzle-kit";
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/main/db/schema.ts",
+    driver: "pglite",
+    dbCredentials: {
+        //database folder
+        url: "./database/"
+    },
+    out: './migrations',
+});
