08520fc264fcc919ccb93a2a4a4add11c3d41886
Cosmo-mcp <gaurav@cosmocp.com>
2025-10-06 06:05:59 +0530

    Update schema.ts

--- src/core/database/schema.ts
+++ src/core/database/schema.ts
@@ -1,7 +1,33 @@
-import {integer, pgTable, smallint} from "drizzle-orm/pg-core";
+import type { InferSelectModel } from "drizzle-orm";
+import {
+    boolean,
+    foreignKey,
+    json,
+    jsonb,
+    pgTable,
+    primaryKey,
+    text,
+    timestamp,
+    uuid,
+    varchar,
+} from "drizzle-orm/pg-core";
 
-//test schema, (TODO) remove
-export const table = pgTable('test', {
-    int: integer(),
-    smallint: smallint()
+export const chat = pgTable("Chat", {
+    id: uuid("id").primaryKey().notNull().defaultRandom(),
+    createdAt: timestamp("createdAt").notNull(),
+    title: text("title").notNull(),
 });
+export type Chat = InferSelectModel<typeof chat>;
+
+export const message = pgTable("Message", {
+    id: uuid("id").primaryKey().notNull().defaultRandom(),
+    chatId: uuid("chatId")
+        .notNull()
+        .references(() => chat.id),
+    role: varchar("role").notNull(),
+    parts: json("parts").notNull(),
+    attachments: json("attachments").notNull(),
+    createdAt: timestamp("createdAt").notNull(),
+});
+
+export type DBMessage = InferSelectModel<typeof message>;
