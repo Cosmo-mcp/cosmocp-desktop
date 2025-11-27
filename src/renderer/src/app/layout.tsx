15b209c3124097491ff30a8d3cf40f51c1746c7c
Cosmo-mcp <gaurav@cosmocp.com>
2025-11-27 18:37:34 +0530

    updated CSS (#59)

--- src/renderer/src/app/layout.tsx
+++ src/renderer/src/app/layout.tsx
@@ -1,7 +1,12 @@
 'use client'
-import ClientLayout from "@/app/client-layout";
 import React from "react";
 import "./globals.css";
+import {inter} from "@/lib/fonts";
+import {ThemeProvider} from "next-themes";
+import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar";
+import {AppSidebar} from "@/components/app-sidebar";
+import {SidebarToggle} from "@/components/sidebar-toggle";
+import {Toaster} from "sonner";
 
 export default async function RootLayout({
                                              children,
@@ -9,10 +14,22 @@ export default async function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={`antialiased`}>
-                <ClientLayout>{children}</ClientLayout>
-            </body>
+        <html lang="en" className={`${inter.variable} antialiased`}>
+        <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <SidebarProvider defaultOpen={true}>
+                <AppSidebar/>
+                <SidebarToggle/>
+                <SidebarInset>{children}</SidebarInset>
+            </SidebarProvider>
+            <Toaster position="top-center"/>
+        </ThemeProvider>
+        </body>
         </html>
     );
 }
