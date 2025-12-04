58af2b92623840aa6a590d7b0952db2c9f736d08
jsuryakt-cosmo <jayasurya@cosmocp.com>
2025-12-05 02:28:41 +0530

    ai-elements update Chat UI and bottom input fix

--- src/renderer/src/app/layout.tsx
+++ src/renderer/src/app/layout.tsx
@@ -30,11 +30,9 @@ export default async function RootLayout({
                 <AppSidebar/>
                 <SidebarInset>
                     <SiteHeader/>
-                    <div className="flex flex-1 flex-col">
-                        <div className="@container/main flex flex-1 flex-col gap-2">
-                            <div className="flex flex-col h-full">
-                                {children}
-                            </div>
+                    <div className="flex flex-1 flex-col h-full w-full">
+                        <div className="@container/main flex flex-1 flex-col h-full w-full">
+                            {children}
                         </div>
                     </div>
                 </SidebarInset>
