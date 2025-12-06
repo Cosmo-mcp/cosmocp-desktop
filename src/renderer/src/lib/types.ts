407d51617b5d328eaa708286b498f64f92ab2180
shashank-cosmo <shashank@cosmocp.com>
2025-12-07 00:01:22 +0530

    - Added ollama to ui (icons and description)

--- src/renderer/src/lib/types.ts
+++ src/renderer/src/lib/types.ts
@@ -20,6 +20,10 @@ export const ProviderInfo: Record<ModelProviderTypeEnum, { name: string; descrip
         name: 'Google',
         description: 'Leverage the powerful Gemini family of models from Google AI.'
     },
+    [ModelProviderTypeEnum.OLLAMA]: {
+        name: 'Ollama',
+        description: 'Access a range of open-source and customizable language models for various AI tasks.'
+    },
     [ModelProviderTypeEnum.CUSTOM]: {
         name: 'Custom',
         description: 'Connect to any OpenAI-compatible API endpoint.'
