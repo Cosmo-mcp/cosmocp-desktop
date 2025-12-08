5fb00e38bf14385fee686fe71a9d1647dcf812bb
jsuryakt-cosmo <jayasurya@cosmocp.com>
2025-12-09 00:44:02 +0530

    add splash screen for loading

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,18 +9,40 @@ import {config} from "dotenv";
 
 export class Main {
     private mainWindow: BrowserWindow | null = null;
+    private splashWindow: BrowserWindow | null = null;
     private readonly isDev = true;
     private readonly MAIN_WINDOW_VITE_DEV_SERVER_URL = this.isDev ? 'http://localhost:3000' : undefined;
 
     constructor() {
         config();
         app.whenReady().then(async () => {
+            await this.createSplashWindow();
             await this.initializeDatabase();
             const ipcHandlerRegistry = container.get<IpcHandlerRegistry>(TYPES.IpcHandlerRegistry);
             ipcHandlerRegistry.registerIpcHandlers();
             await this.createWindow();
             this.registerAppEvents();
         });
+    }
+
+    private async createSplashWindow(): Promise<void> {
+        this.splashWindow = new BrowserWindow({
+            width: 400,
+            height: 400,
+            frame: false,
+            transparent: true,
+            alwaysOnTop: true,
+            resizable: false,
+            skipTaskbar: true,
+            webPreferences: {
+                contextIsolation: true,
+                nodeIntegration: false,
+            },
+        });
+
+        this.splashWindow.setMenuBarVisibility(false);
+        await this.splashWindow.loadFile(path.join(__dirname, 'splash.html'));
+        this.splashWindow.center();
     }
 
     private async initializeDatabase(): Promise<void> {
@@ -46,6 +68,7 @@ export class Main {
         this.mainWindow = new BrowserWindow({
             height: 600,
             width: 800,
+            show: false,
             webPreferences: {
                 contextIsolation: true,
                 nodeIntegration: false,
@@ -56,16 +79,24 @@ export class Main {
 
         this.mainWindow.setMenuBarVisibility(false);
 
+        this.mainWindow.webContents.once('did-finish-load', () => {
+            if (this.splashWindow) {
+                this.splashWindow.close();
+                this.splashWindow = null;
+            }
+            this.mainWindow?.show();
+
+            if (this.isDev) {
+                this.mainWindow?.webContents.openDevTools();
+                this.mainWindow?.maximize();
+            }
+        });
+
         if (this.MAIN_WINDOW_VITE_DEV_SERVER_URL) {
             await this.mainWindow.loadURL(this.MAIN_WINDOW_VITE_DEV_SERVER_URL);
         } else {
             await this.mainWindow.loadFile(path.join(__dirname, `../renderer/main_window/index.html`));
         }
-        if (this.isDev) {
-            this.mainWindow.webContents.openDevTools();
-            this.mainWindow.maximize();
-        }
-
     }
 
     private registerAppEvents(): void {
