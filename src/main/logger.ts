9e81d179fbb5aa3acf0f971012a96b7ee0cd432a
Cosmo-mcp <gaurav@cosmocp.com>
2026-01-21 06:05:23 +0530

    Update logger.ts

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -4,10 +4,10 @@ import {app} from "electron";
 
 log.initialize();
 
-//const logDir = path.join(app.getPath("userData"), "logs");
+const logDir = path.join(app.getPath("userData"), "logs");
 
 log.transports.file.level = 'info';
 log.transports.file.maxSize = 5 * 1024 * 1024; // 5MB
-//log.transports.file.resolvePathFn = () => path.join(logDir, "cosmo.log");
+log.transports.file.resolvePathFn = () => path.join(logDir, "cosmo.log");
 
 export const logger = log.scope("main");
