b16b965d676ee986803c4fec7a6db206c53c84bc
Cosmo-mcp <gaurav@cosmocp.com>
2026-01-21 06:34:24 +0530

    Delete 0018_persona_spark.sql

--- migrations/0018_persona_spark.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "Persona" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updatedAt" timestamp DEFAULT now() NOT NULL,
-	"name" text NOT NULL,
-	"details" text NOT NULL
-);
-CREATE UNIQUE INDEX "Persona_name_unique" ON "Persona" ("name");
