dfce8d89f32090f671f38459ea5a2fc313d2b177
Cosmo-mcp <gaurav@cosmocp.com>
2026-02-24 07:09:59 +0530

    updated logos

---
+++ src/renderer/src/components/provider-icon.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {ModelProviderTypeEnum} from "core/database/schema/modelProviderSchema";
+import {cn} from "@/lib/utils";
+
+type ThemeType = "light" | "dark";
+
+type ProviderIconProps = {
+    type: ModelProviderTypeEnum;
+    size?: number;
+    theme?: string;
+    className?: string;
+};
+
+const ProviderIcon = ({ type, size = 20, theme, className }: ProviderIconProps) => {
+    const iconPath = "providers/" + type + ".svg";
+
+    return (
+        <img
+            title={type}
+            src={iconPath}
+            alt={`${type} icon`}
+            width={size}
+            height={size}
+            style={{ backgroundColor: theme === "dark" ? "white" : "transparent" }}
+            className={cn("mr-2 rounded-sm", className)}
+        />
+    );
+};
+
+export default ProviderIcon;
